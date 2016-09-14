faab8645c347799f6a5d35a93c60a56e95ff7a60
Rares Ambrus <raambrus@kth.se>
2016-09-14 21:33:22 +0200

    Some small fixes

--- scripts/direct_ptu_server.py
+++ scripts/direct_ptu_server.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 
 import numpy as np
-from geometry_msgs import Pose, PoseStamped
+from geometry_msgs.msg import Pose, PoseStamped
 import tf
 import rospy
 import math
-from scitos_ptu import PtuGotoAction, PtuGotoGoal,  PtuGotoResult
+from scitos_ptu.msg import PtuGotoAction, PtuGotoGoal,  PtuGotoResult
+from actionlib_msgs.msg import *
+import actionlib
 
 class DirectPTUServer(object):
 
@@ -14,16 +16,17 @@ class DirectPTUServer(object):
         rospy.init_node('direct_ptu_server')
         rospy.Subscriber("/direct_ptu_at_point", Pose, self.direct_ptu_at)
 
-        self.t = tf.TransformerROS(True, rospy.Duration(10.0))
+        self.t = tf.TransformerROS(True, rospy.Duration(4.0))
         self.action_client = actionlib.SimpleActionClient('/SetPTUState', PtuGotoAction)
         rospy.loginfo("Waiting for ptu action...")
         self.action_client.wait_for_server()
+        rospy.loginfo("Got ptu action server")
 
     def direct_ptu_at(self, p):
 
         msg = PoseStamped()
         msg.pose = p
-        msg.header.frame_id = "/map
+        msg.header.frame_id = "/map"
         msg.header.stamp = rospy.Time.now()
         msgq = self.t.transformPose("/ptu_pan_motor", msg)
 
