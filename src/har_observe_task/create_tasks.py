fd18450ab72c966d99771feaae38d290ba2ccc60
Hakan <hkaraoguz@yahoo.com.tr>
2017-01-24 18:23:27 +0100

    Fixed roi indexing

--- src/har_observe_task/create_tasks.py
+++ src/har_observe_task/create_tasks.py
@@ -54,11 +54,11 @@ def create_har_sweep_tasks(roi_db_name,roi_collection_name,roi_config):
             distances.append(distance(roi_position,node_position))
         minindex =  distances.index(min(distances))
         roinodepairs.append((roi.id, topological_nodes[minindex]))
-    print roinodepairs
+    #print roinodepairs
     tasks = dict()
     for pair in roinodepairs:
         task = Task(start_node_id=pair[1].name, action='do_sweep', max_duration=rospy.Duration(60 * 2))
         task_utils.add_string_argument(task, 'medium')
-        tasks[roi.id] = task
-    print tasks
+        tasks[pair[0]] = task
+    print tasks.keys()
     return tasks
